54ce42374d9ac6e8d3ac2dfa7cc93f99e55b60e1
fateme <fatemehkamarei99@gmail.com>
2025-09-03 16:50:41 +0330

    remove direct import of logo image

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,11 +1,15 @@
 import Link from "next/link";
 import Image from "next/image";
-import logo from "@/public/assets/images/Logo.png";
 
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
-      <Image src={logo} height={35} width={89} alt="auto-rent" />
+      <Image
+        src="/assets/images/Logo.png"
+        height={35}
+        width={89}
+        alt="auto-rent logo"
+      />
     </Link>
   );
 }
